bda47a4e8d9f067386a7adfdeb1753576b423b3d
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-19 08:15:42 +0600

    project initialized with necessary dependencies and components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Account from "./Account";
+import Profile from "./Profile";
+
+export { Header, Footer, Home, Login, Register, Account, Profile };
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, Header, Home } from "../components";
+
+const Main = () => {
+   return (
+      <div>
+         <Header />
+         <Home />
+         <Footer />
+      </div>
+   );
+};
+
+export default Main;
